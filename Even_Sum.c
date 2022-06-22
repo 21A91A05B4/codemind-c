5273c60345ec2413d792a519d6efb1c41acd3bb4
21A91A05B4 <97424373+21A91A05B4@users.noreply.github.com>
2022-06-22 10:52:12 +0530

    #include<stdio.h> #include<math.h> int main() {     int a[100],f,s=0,c=0,n,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             s=s+a[i];         }     }     printf("%d",s);
    }

---
+++ Even_Sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a[100],f,s=0,c=0,n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            s=s+a[i];
+        }
+    }
+    printf("%d",s);
+    
+}
