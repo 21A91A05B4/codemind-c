7e609269147adc7b25ca8fdc383dddc27c2ae6a4
21A91A05B4 <97424373+21A91A05B4@users.noreply.github.com>
2022-06-28 21:10:10 +0530

    #include<stdio.h> int main() {     char str[20];     int i,c=0,f=0,t,T;     scanf("%d",&T);     for(t=1;t<=T;t++)     {         scanf("%s",str);         c=0;f=0;     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>='0' && str[i]<='9')         {             c++;         }         else         {             f++;         }     }     if(f>0)     {         printf("False ");     }     else     {         printf("True ");     }     }
    }

---
+++ String_decimal_.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    char str[20];
+    int i,c=0,f=0,t,T;
+    scanf("%d",&T);
+    for(t=1;t<=T;t++)
+    {
+        scanf("%s",str);
+        c=0;f=0;
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>='0' && str[i]<='9')
+        {
+            c++;
+        }
+        else
+        {
+            f++;
+        }
+    }
+    if(f>0)
+    {
+        printf("False
+");
+    }
+    else
+    {
+        printf("True
+");
+    }
+    }
+
+}
